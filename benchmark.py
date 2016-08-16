60356163a63dbdca9bd1b3d46ef117f0411a5c83
wolfram74 <phaugen@gmail.com>
2016-08-16 17:28:51 -0500

    reran benchmarks with IO removed from loop, wrote summary of results

--- benchmark.py
+++ benchmark.py
@@ -3,16 +3,27 @@ import fast
 import simple
 
 fast_start = time.time()
+sets, trie = fast.import_words()
 for cycle in range(10):
-    sets, trie = fast.import_words()
     composites = fast.find_composites(sets, trie)
 fast_end = time.time()
 
 simple_start = time.time()
+sets = simple.import_words()
 for cycle in range(10):
-    sets = simple.import_words()
     composites = simple.find_composites(sets)
 simple_end = time.time()
 
 print(fast_end-fast_start)
 print(simple_end-simple_start)
+'''
+results:
+with looading in the loop,
+fast 3.01 seconds
+simple 5.75 second
+loading outside the loop
+fast 1.26 seconds
+simple 4.73 seconds
+so fast took 1.8~ seconds for 10 load cycles
+while simple took a little under 1, however, composite finding is close to 4 times faster with the trie method.
+'''
